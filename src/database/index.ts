71b80aed2d6b74b9a2b35e9aaa89dce3db0b9e18
Adams Figueroa de Oliveira <adamsfo20@gmail.com>
2024-08-21 22:51:53 +0000

    Implementação de todos os modelos de acordo com o Et-01

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,6 +3,11 @@ const dbConfig = require('../config/database')
 import { EmpresaInit } from '../models/Empresa';
 import { UsuarioInit } from '../models/Usuario'
 import { CidadeInit } from '../models/Cidade';
+import { VeiculoInit } from '../models/Veiculo';
+import { ClienteFornecedorInit } from '../models/ClienteFornecedor';
+import { ContaAPagarInit } from '../models/ContaAPagar';
+import { ContaAReceberInit } from '../models/ContaAReceber';
+import { ContaBancariaInit } from '../models/ContaBancaria';
 const ConfigIniciais = require('./ConfigIniciais')
 
 const connection = new Sequelize(dbConfig);
@@ -17,8 +22,14 @@ const connection = new Sequelize(dbConfig);
     UsuarioInit(connection)
     CidadeInit(connection)
     EmpresaInit(connection)
+    VeiculoInit(connection)
+    ClienteFornecedorInit(connection)
+    ContaAPagarInit(connection)
+    ContaAReceberInit(connection)
+    ContaBancariaInit(connection)
     
     // Sincronizando os modelos com o banco de dados        
+    // await connection.sync();
     await connection.sync({ alter: true });
     
     // Executando configurações iniciais
